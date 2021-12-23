7ac4c415f2e763691f6433cf9081da0a61809c09
jtownshend <jtownshend@mdsol.com>
2021-12-23 12:25:28 +0000

    Monads pt2

--- src/main/scala/com/rockthejvm/part2advancedfp/Monads.scala
+++ src/main/scala/com/rockthejvm/part2advancedfp/Monads.scala
@@ -1,5 +1,6 @@
 package com.rockthejvm.part2advancedfp
 
+import scala.annotation.targetName
 object Monads {
 
   val alist = List(1,2,3)
@@ -46,17 +47,92 @@ object Monads {
 
     // prop 3: associativity
     val associativity = anOption.flatMap(f).flatMap(g) == anOption.flatMap(x => f(x).flatMap(g))
-
-    def main(args: Array[String]): Unit = {
-
-    }
-
-
   }
 
 
-  def main(args: Array[String]): Unit = {
+// exercise - is this a monad or not?
+  case class PossiblyMonad[A](unsafeRun: () => A) {
+    def map[B](f: A => B): PossiblyMonad[B] =
+      PossiblyMonad(() => f(unsafeRun()))
 
+
+    def flatMap[B](f: A => PossiblyMonad[B]): PossiblyMonad[B] =
+      PossiblyMonad(() => f(unsafeRun()).unsafeRun())
+  }
+
+  object PossiblyMonad {
+    @targetName("pure")
+    def apply[A](value: => A): PossiblyMonad[A] =
+      new PossiblyMonad(() => value)
+  }
+
+  // IT IS A MONAD!
+  def possiblyMonadStory(): Unit = {
+    val aPossiblyMonad = PossiblyMonad(42)
+    val f = (x: Int) => PossiblyMonad(x + 1)
+    val g = (x: Int) => PossiblyMonad(2 * x)
+    val pure = (x: Int) => PossiblyMonad(x)
+
+    // prop 1: left identity -> pure applied on a value + flatmap any function == function invoked on that value
+    val prop1 = pure(42).flatMap(f) == f(42)
+
+    // prop 2: right identity -> given a possible monad, flat mapping with pure == that possible monad
+    val prop2 = aPossiblyMonad.flatMap(pure) == aPossiblyMonad
+
+    // prop 3: associativity
+    val prop3 = aPossiblyMonad.flatMap(f).flatMap(g) == aPossiblyMonad.flatMap(x => f(x).flatMap(g))
+
+    // false negatives
+    println(prop1)
+    println(prop2)
+    println(prop3)
+
+    // real tests: values produced
+    val prop1_1 = pure(42).flatMap(f).unsafeRun() == f(42).unsafeRun()
+    val prop2_1 = aPossiblyMonad.flatMap(pure).unsafeRun() == aPossiblyMonad.unsafeRun()
+    val prop3_1 = aPossiblyMonad.flatMap(f).flatMap(g).unsafeRun() == aPossiblyMonad.flatMap(x => f(x).flatMap(g)).unsafeRun()
+
+    println(prop1_1)
+    println(prop2_1)
+    println(prop3_1)
+  }
+
+  // the values inside wont print if we run => the monad descirbes the computations but it wont perform them
+  // side effects will not be performed => seperates the description of computation from performace of computation
+  // that is why we have the unsafe run method.
+  def possiblyMonadExample(): Unit = {
+    val aPossiblyMonad = PossiblyMonad {
+      println("doing some stuffs")
+      42
+    }
+
+    val aPossiblyMonad2 = PossiblyMonad {
+      println("doing some more stuffs") // doesnt print
+      "Scala"
+    }
+
+    val aForComp = for {
+      number <- aPossiblyMonad
+      string <- aPossiblyMonad2
+    } yield println(s"$number-$string")
+
+    aPossiblyMonad.unsafeRun()
+    aForComp // wont print
+//    aForComp.unsafeRun() // will print but also prints everything that it calls :-/
+  }
+
+  // so we can do pure FP on any computations that might perform side effects
+  // so we can chain or compose computations that might cause side effects
+  // and at the end of app we can call unsafeRun on the final monad instance
+  // unsafeRun sits at the bottom of libraries like ZIO or CATS
+  // PossiblyMonad type is actually called IO!
+  // this exercise is a vast simplification of the IO in CATS or ZIO
+
+
+  def main(args: Array[String]): Unit = {
+    possiblyMonadExample()
+
+    possiblyMonadStory()
   }
 
 }
