4bb862ff6b4f8613ca71e4e50206bb208487328a
jtownshend <jtownshend@mdsol.com>
2022-01-04 11:13:04 +0000

    parallel programming problems

--- src/main/scala/com/rockthejvm/part3async/JVMConcurrencyProblems.scala
+++ src/main/scala/com/rockthejvm/part3async/JVMConcurrencyProblems.scala
@@ -2,8 +2,9 @@ package com.rockthejvm.part3async
 
 object JVMConcurrencyProblems {
 
+  // vars are root of all evil when running in parallel
   def runInParallel(): Unit = {
-    var x = 0
+    var x = 0 // one issue is mutable values = bad
 
     val thread1 = new Thread(() => {
       x = 1
@@ -13,69 +14,46 @@ object JVMConcurrencyProblems {
       x = 2
     })
 
+    // start these at (roughly) the same time
     thread1.start()
     thread2.start()
-    println(x) // race condition
+
+    // if this method is run enough times eventually a scenario will be reached
+    // where despite thread1 starting first, it ends last meaning that x = 1
+    println(x)
+    // this is called a race condition
   }
 
   case class BankAccount(var amount: Int)
 
-  def buy(bankAccount: BankAccount, thing: String, price: Int): Unit = {
-    /*
-      involves 3 steps:
-      - read old value
-      - compute result
-      - write new value
-     */
-    bankAccount.amount -= price
+  def buy(account: BankAccount, thing: String, price: Int): Unit = {
+    account.amount -= price
   }
 
-  def buySafe(bankAccount: BankAccount, thing: String, price: Int): Unit = {
-    bankAccount.synchronized { // does not allow multiple threads to run the critical section AT THE SAME TIME
-      bankAccount.amount -= price // critical section
+  def buySafe(account: BankAccount, thing: String, price: Int): Unit = {
+    account.synchronized { // does not allow multiple theads to run critical section at same time :-)
+      account.amount -=  price // critical section
     }
   }
 
-  /*
-    Example race condition:
-    thread1 (shoes)
-      - reads amount 50000
-      - compute result 50000 - 3000 = 47000
-    thread2 (iPhone)
-      - reads amount 50000
-      - compute result 50000 - 4000 = 46000
-    thread1 (shoes)
-      - write amount 47000
-    thread2 (iPhone)
-      - write amount 46000
-   */
   def demoBankingProblem(): Unit = {
-    (1 to 10000).foreach { _ =>
-      val account = new BankAccount(50000)
+    (1 to 10000).foreach {_ =>
+      val account = BankAccount(50000)
       val thread1 = new Thread(() => buy(account, "shoes", 3000))
       val thread2 = new Thread(() => buy(account, "iPhone", 4000))
+
       thread1.start()
       thread2.start()
       thread1.join()
       thread2.join()
-      if (account.amount != 43000) println(s"AHA! I've just broken the bank: ${account.amount}")
+
+      if (account.amount != 43000) println(s"I've broken the account ${account.amount}")
     }
   }
 
-  /**
-    Exercises
-    1 - create "inception threads"
-      thread 1
-        -> thread 2
-            -> thread 3
-                ....
-      each thread prints "hello from thread $i"
-      Print all messages IN REVERSE ORDER
 
-    2 - What's the max/min value of x?
-    3 - "sleep fallacy": what's the value of message?
-   */
-  // 1 - inception threads
+  // exercise: inception threads TODO
+
   def inceptionThreads(maxThreads: Int, i: Int = 1): Thread =
     new Thread(() => {
       if (i < maxThreads) {
@@ -83,58 +61,10 @@ object JVMConcurrencyProblems {
         newThread.start()
         newThread.join()
       }
-      println(s"Hello from thread $i")
+      println(s"hello from thead ${i}")
     })
-
-  // 2
-  /*
-    max value = 100 - each thread increases x by 1
-    min value = 1
-      all threads read x = 0 at the same time
-      all threads (in parallel) compute 0 + 1 = 1
-      all threads try to write x = 1
-   */
-  def minMaxX(): Unit = {
-    var x = 0
-    val threads = (1 to 100).map(_ => new Thread(() => x += 1))
-    threads.foreach(_.start())
-  }
-
-  // 3
-  /*
-    almost always, message = "Scala is awesome"
-    is it guaranteed? NO
-    Obnoxious situation (possible):
-
-    main thread:
-      message = "Scala sucks"
-      awesomeThread.start()
-      sleep(1001) - yields execution
-    awesome thread:
-      sleep(1000) - yields execution
-    OS gives the CPU to some important thread, takes > 2s
-    OS gives the CPU back to the main thread
-    main thread:
-      println(message) // "Scala sucks"
-    awesome thread:
-      message = "Scala is awesome"
-   */
-  def demoSleepFallacy(): Unit = {
-    var message = ""
-    val awesomeThread = new Thread(() => {
-      Thread.sleep(1000)
-      message = "Scala is awesome"
-    })
-
-    message = "Scala sucks"
-    awesomeThread.start()
-    Thread.sleep(1001)
-    // solution: join the worker thread
-    awesomeThread.join()
-    println(message)
-  }
 
   def main(args: Array[String]): Unit = {
-    demoSleepFallacy()
+    inceptionThreads(50).start()
   }
 }
